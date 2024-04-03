74d531e46d7c06a14bcf1dcb744302485dda2838
n8hadden <n8hadden888@gmail.com>
2024-04-03 14:27:07 -0700

    view, edit, and delete all work now

--- studentSystem/home.php
+++ studentSystem/home.php
@@ -31,7 +31,7 @@
                         echo " <td > {$email}</td>";
                         echo " <td class='text-center'> <a href='view.php?&id={$student_id}' class='btn btn-primary'> <i class='bi bi-eye'></i> View</a></td>";
                         echo " <td class='text-center'> <a href='update.php?edit&id={$student_id}' class='btn btn-secondary'> <i class='bi bi-pencil'></i> EDIT</a></td>";
-                        echo " <td class='text-center'> <a href='update.php?&delete&id={$student_id}' class='btn btn-danger'> <i class='bi bi-trash'></i> DELETE</a></td>";
+                        echo " <td class='text-center'> <a href='delete.php?&delete&id={$student_id}' class='btn btn-danger'> <i class='bi bi-trash'></i> DELETE </a></td>";
                         echo " </tr>";
                     }
                 ?>
@@ -39,3 +39,5 @@
         </tbody>
     </table>
 </div>
+
+<?php include "footer.php" ?>
